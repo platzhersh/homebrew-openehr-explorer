1438704ac3e9bfd5cdd024e9eef87c6e9a87a733
Chregi <1445827+platzhersh@users.noreply.github.com>
2026-09-08 18:51:24 +0000

    chore: update openehr-explorer to 0.9.0

--- Casks/openehr-explorer.rb
+++ Casks/openehr-explorer.rb
@@ -1,11 +1,11 @@
 cask "openehr-explorer" do
-  version "0.8.0"
-  sha256 "68341341ecff576b11fcfb4bf3c96c0ca97faa2139fd62c830d79dd1679fcd74"
+  version "0.9.0"
+  sha256 "f1761a7518f010ab65ff9c9ad5de895f7b86d376c400e475f6c994c34883b8b9"
 
   url "https://github.com/platzhersh/openehr-explorer/releases/download/v#{version}/openEHR.Explorer_#{version}_universal.dmg"
   name "openEHR Explorer"
   desc "Browse, query, and inspect any openEHR CDR"
-  homepage "https://platzhersh.github.io/openehr-explorer/"
+  homepage "https://openehr-explorer.dev/"
 
   depends_on macos: :catalina
 
